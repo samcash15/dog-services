7e8dfb367d24daca69da284d0957287540b8eab3
samcash15 <cashionsam15@gmail.com>
2024-01-17 14:48:05 -0600

    Implementing swagger documentation annotations and updating my context path.

--- src/main/java/com/cashion/dog/dogservices/config/SwaggerConfig.java
+++ src/main/java/com/cashion/dog/dogservices/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.cashion.dog.dogservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -9,11 +10,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SwaggerConfig {
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.cashion.dog")).build();
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cashion.dog.dogservices"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
--- src/main/java/com/cashion/dog/dogservices/restservice/breed/controller/DogBreedController.java
+++ src/main/java/com/cashion/dog/dogservices/restservice/breed/controller/DogBreedController.java
@@ -4,6 +4,8 @@ import com.cashion.dog.dogservices.persistence.domain.DogBreed;
 import com.cashion.dog.dogservices.persistence.repository.DogBreedRepository;
 import com.cashion.dog.dogservices.restservice.breed.resource.DogBreedResource;
 import com.cashion.dog.dogservices.restservice.breed.service.DogBreedService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(value = "Dog Breed API")
 @RestController
 public class DogBreedController {
     public static final Log LOG = LogFactory.getLog(DogBreedController.class);
@@ -28,6 +31,7 @@ public class DogBreedController {
         this.dogBreedRepository = dogBreedRepository;
     }
 
+    @ApiOperation(value = "Add a new dog breed")
     @PostMapping(path = "/addBreed", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Object> addBreed(@Valid @RequestBody DogBreedResource dogBreedResource,
                                                   BindingResult bindingResult) {
@@ -41,6 +45,7 @@ public class DogBreedController {
         return ResponseEntity.ok("");
     }
 
+    @ApiOperation(value = "Get information about a specific dog breed.")
     @GetMapping(path = "/getBreed/{breed}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<DogBreed> getBreed(@PathVariable String breed) {
         DogBreed dogBreed = dogBreedService.getBreed(breed);
